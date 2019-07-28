753d722a2fa90fc1ca66438d9d788699edf26faf
unknown <Nagarro@harme3145860-02.Nagarro.local>
2019-07-28 17:58:16 +0530

    first push

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const PORT = process.env.PORT || 4200;
+var request = require('request');
+
+const app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.use(express.urlencoded({
+    extended: true
+    })); 
+
+app.use(express.static('kratos'))
+app.use(express.json());           
+
+app.get('/*', function(req, res) { 
+res.sendFile(__dirname + '/demo-app/index.html')
+});
+
+app.listen(PORT, () => console.log('UI service listening on port '+PORT+'!')) 
+    
